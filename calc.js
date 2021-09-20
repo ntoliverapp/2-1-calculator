3a9d75cd95eb7b774e712429263f18aa538d52f2
ntoliverapp <n.toliverapplication@gmail.com>
2021-09-20 14:58:16 -0700

    Multiplication implementation working

--- calc.js
+++ calc.js
@@ -22,6 +22,10 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 - num2);
   }	
 
+	if (mathSymbol === "*"){
+		console.log(num1 * num2);
+  }	
+
 	console.log('mathSymbol', mathSymbol);
             console.log('num1', num1);
             console.log('num2', num2);
